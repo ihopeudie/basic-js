e3df94f07b8131e5597d3d5121b85572b94ab774
Aleksey Sverbeev <aleksey_sverbeev@epam.com>
2020-05-29 23:23:00 +0300

    half-life 3 created

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,14 @@
-const CustomError = require("../extensions/custom-error");
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
+const RATE = 0.693 / HALF_LIFE_PERIOD;
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
-
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+    if (!sampleActivity || sampleActivity.toString() !== sampleActivity) {
+        return false;
+    }
+    const activity = parseFloat(sampleActivity);
+    if (isNaN(activity) || activity <= 0 || activity >= MODERN_ACTIVITY) {
+        return false;
+    }
+    return Math.ceil(Math.log(MODERN_ACTIVITY / Number(activity)) / RATE);
 };
